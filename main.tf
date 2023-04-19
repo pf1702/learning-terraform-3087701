d9b3c43b04019ea8d3a84d61792c52a9652a0f12
pf1702 <130201885+pf1702@users.noreply.github.com>
2023-04-18 20:42:28 -0700

    Created Security Group

--- main.tf
+++ main.tf
@@ -17,9 +17,12 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+
 resource "aws_instance" "blog" {
+
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+}
 
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
