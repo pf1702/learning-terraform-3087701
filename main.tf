0afd130ab5bf3dd1813780b9a0ede5da181a02fd
pf1702 <130201885+pf1702@users.noreply.github.com>
2023-04-20 12:07:52 -0700

    Remove private subnet

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  private_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  azs                    = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  create_private_subnets = false
 
   enable_nat_gateway = true
 
