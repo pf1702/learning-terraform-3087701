0bdf99b787c5c27464f68282bc94c67dde8edb8c
pf1702 <130201885+pf1702@users.noreply.github.com>
2023-04-20 11:35:24 -0700

    create dev vpc and route table

--- main.tf
+++ main.tf
@@ -31,21 +31,21 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_route_table" "blog_sg_route" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "blog_sg" {
+  vpc_id = module.blog_vpc.vpc_id
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.blog_sg.id
   }
 
   route {
     ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+    egress_only_gateway_id = aws_egress_only_internet_gateway.blog_sg.id
   }
 
   tags = {
-    Name = "example"
+    Name = "Blog_SG"
   }
 }
 
