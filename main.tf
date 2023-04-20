fe5a9ab1fe034b23ddc09eaa1475a74f3ce32125
pf1702 <130201885+pf1702@users.noreply.github.com>
2023-04-20 11:03:50 -0700

    Create dev vpc

--- main.tf
+++ main.tf
@@ -34,6 +34,9 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
@@ -46,7 +49,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
 
-  name   = "blog_new"
+  name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
