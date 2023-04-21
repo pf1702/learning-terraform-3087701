3f2931b1b828df79dacbde881177fb6f7740f1e1
pf1702 <130201885+pf1702@users.noreply.github.com>
2023-04-21 13:58:04 -0700

    load balncer

--- main.tf
+++ main.tf
@@ -33,30 +33,32 @@ module "blog_vpc" {
 }
 
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
-module "blog-alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
-  name = "my-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-
-  }
 
   target_groups = [
     {
@@ -66,7 +68,7 @@ module "blog-alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.blog.id
+          target_id = aws_instance
           port = 80
         }
       }
